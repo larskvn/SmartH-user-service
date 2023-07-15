7ed0cf54b58cc58cb616333217d6de4666405c86
lars kvn <kvnyarbis@gmail.com>
2023-07-15 00:02:30 -0500

    config url result

--- src/main/java/com/smarthealth/feingClients/ResultFeingClient.java
+++ src/main/java/com/smarthealth/feingClients/ResultFeingClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "result-service", url = "https://smarth-resultado-service.azurewebsites.net", path = "/resultado")
+@FeignClient(name = "result-service", url = "https://smart-resultados.azurewebsites.net", path = "/resultado")
 public interface ResultFeingClient {
 
     @PostMapping()
